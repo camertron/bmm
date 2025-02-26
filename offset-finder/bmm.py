82e64ccc527265865cd153338459c797a07189b9
Cameron Dutro <camertron@gmail.com>
2025-02-26 11:19:57 -0800

    Improve algorithm to find all 3 segments

--- offset-finder/bmm.py
+++ offset-finder/bmm.py
@@ -4,66 +4,46 @@ from scipy import signal
 import argparse
 import json
 
+def main(within_file):
+    find_file = "./bmm.wav"
+    y_within, sr = librosa.load(within_file, sr=None)
+    y_find, _ = librosa.load(find_file, sr=sr)
+    peaks = []
+    candidates = [(0, len(y_within))]
 
-def find_best_peak(y_within, y_find, first_chunk, second_chunk):
-    first_start_at, first_end_at = first_chunk
-    c1 = signal.correlate(y_within[first_start_at:first_end_at], y_find, mode='valid', method='fft')
+    for i in range(0, 5):
+        new_candidates = []
 
-    second_start_at, second_end_at = second_chunk
-    c2 = signal.correlate(y_within[second_start_at:second_end_at], y_find, mode='valid', method='fft')
+        for candidate in candidates:
+            start = candidate[0]
+            end = candidate[1]
+            c = signal.correlate(y_within[start:end], y_find, mode='valid', method='fft')
+            peak = np.argmax(c)
+            peaks.append((peak + start, c))
 
-    if np.max(c1) > np.max(c2):
-        return np.argmax(c1) + first_start_at
-    else:
-        return np.argmax(c2) + second_start_at
+            # for some reason, signal.correlate can return results outside the start..end bounds
+            if peak > start and peak < end:
+                new_candidates.append((start, peak - (1 * sr)))
+                new_candidates.append((peak + (1 * sr), end))
 
+        candidates = new_candidates
 
-def find_peak(y_within, y_find, start_at=None, end_at=None):
-    c = signal.correlate(y_within[start_at:end_at], y_find, mode='valid', method='fft')
-    peak = np.argmax(c)
+    peaks = [peak for peak in peaks if np.max(peak[1]) > 500]
 
-    return peak + start_at
+    output = []
 
+    for peak in peaks:
+        offset = peak[0]
+        c = peak[1]
+        sec = round(offset / sr, 2)
+        output.append({"offset": sec, "hr": hr(sec), "score": int(np.max(c))})
+
+    print(json.dumps(output))
 
 def hr(sec):
     m = sec // 60
     s = sec % 60
     return f"{int(m)}m{int(s)}s"
-
-
-def main(within_file):
-    find_file = "/app/bmm.wav"
-    y_within, sr = librosa.load(within_file, sr=None)
-    y_find, _ = librosa.load(find_file, sr=sr)
-
-    first_peak = None
-    second_peak = None
-
-    try:
-        first_peak = find_peak(y_within, y_find, 0, -1)
-    except:
-        pass
-
-    if first_peak:
-        try:
-            first_chunk = (0, first_peak - (5 * sr))
-            second_chunk = (first_peak + (5 * sr), -1)
-            second_peak = find_best_peak(y_within, y_find, first_chunk, second_chunk)
-        except:
-            pass
-
-    results = []
-
-    if first_peak:
-        sec = round(first_peak / sr, 2)
-        results.append({"offset": sec, "hr": hr(sec)})
-
-        if second_peak:
-            sec = round(second_peak / sr, 2)
-            results.append({"offset": sec, "hr": hr(sec)})
-
-    print(json.dumps(results))
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
